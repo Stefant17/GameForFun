74c2fd67c80f12a4f2ebe7acf239116e6b2200f0
stebbi <stefant17@ru.is>
2019-02-05 14:07:08 +0000

    starting point, added main loop and started combat mechanincs

--- program/main.py
+++ program/main.py
@@ -8,10 +8,15 @@ white = (255,255,255)
 black = (0,0,0)
 red = (255,0,0)
 yellow = (255, 255, 0)
+#main caracter equipment
+#             name,  health, weapon_attack, health_potions, mana_potions, Spells 
+caracter_info["stefan",  100,  10 , 3 , 2 , [heal, fireball, arcane missles]]
+
 
 # initial enemy location for developmennt
-enemy_x = 200
-enemy_y = 200
+#             x     y  id     name  lvl health  attak 
+enemyList = [[[60, 500], 1 , "dragon" , 2  , 200    , 20] ]
+enemy_available = []
 
 def initialize():
 #  amount = input('how manny caracters?')
@@ -46,6 +51,7 @@ def create_caracter():
 
 def main(): 
 ##  initialize()
+  global enemyList
   x_cord = 67 
   y_cord = 540
   gameDisplay.fill(white)
@@ -74,28 +80,55 @@ def main():
         elif event.key == pygame.K_UP:
           y_cord = y_cord - 5
           movement(x_cord, y_cord)
+    if (check_if_hit(x_cord,y_cord)):
+      combat(1)
+      enemyList[0][0][0] = 0
+      enemyList[0][0][1] = 0
 
 def movement(X_cord, y_cord):
+  global enemyList
   gameDisplay.fill(white)
-  pygame.draw.rect(gameDisplay, red, [X_cord , y_cord, 50, 50])
+  pygame.draw.rect(gameDisplay, red, [ X_cord, y_cord , 30, 30])
+  pygame.draw.rect(gameDisplay, red, [enemyList[0][0][0], enemyList[0][0][1] , 20, 20])
   pygame.display.update()
 
-def combat(enemy): 
+def combat(enemy_id): 
+  global enemyList
+  monster_health = 0
+  lvl = 0
+  monster_name = "a"
+  monster_attack = 0 
+  for i in enemyList:
+    if i[1] == enemy_id:
+      monster_name = i[2]
+      lvl = i[3]
+      monster_health = i[4]
+      monster_attack = i[5]
   gameDisplay.fill(white)
   pygame.display.update()
-  weaponDamage = 10
   MonsterHealth = 100 
   PlayerHealth = 100
   winner = False
+  print(monster_name)
   while (winner == False):
     for event in pygame.event.get():
       if event.type == pygame.KEYDOWN:
         if event.key == pygame.K_s:
           monsterHealth -= weaponDamage
           if monsterHealth <= 0:
-            winner == True
+            winner = True
         if event.key == pygame.K_p:
           player_health += 50
+        if event.key == pygame.K_q:
+          winner = True
+
+def check_if_hit(x_cord, y_cord):
+  global enemyList
+  for i in enemyList:
+    if x_cord > i[0][0] and x_cord < i[0][0]+35 or x_cord + 30 > i[0][0] and x_cord + 30 < i[0][0] + 35:
+      if y_cord > i[0][1] and y_cord < i[0][1]+ 35 or y_cord+35 > i[0][1] and y_cord + 30 < i[0][1] + 35:
+        return True
+
 
 
 
